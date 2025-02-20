0563dbcccabf85cd8f1f2bd1e431dc17a8e0398a
azeezolasunkanmi <azeezolasunkanmi99@yahoo.com>
2025-02-20 02:44:48 +0100

    added mobile nav

--- components/MainNavigation/MainNavigation.tsx
+++ components/MainNavigation/MainNavigation.tsx
@@ -11,10 +11,10 @@ const MainNavigation = () => {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
-
+  const closeMenu = () => setIsOpen(false);
   return (
-    <header className="sticky top-0 left-0 backdrop-blur-sm z-20 bg-light">
-      <div className="container">
+    <header className="sticky top-0 left-0  z-20 bg-light shadow-md">
+      <div className="container relative">
         <div className="flex items-center justify-between p-4">
           <Image src={logo} alt="Enstore logo" width={120} />
 
@@ -44,47 +44,46 @@ const MainNavigation = () => {
           </nav>
         </div>
       </div>
+
+      {/* Backdrop (Closes Menu on Click) */}
+      {isOpen && (
+        <div
+          className="fixed inset-0 bg-black bg-opacity-50 z-30"
+          onClick={closeMenu} // Clicking outside closes menu
+        ></div>
+      )}
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-0 right-0 w-3/4 max-w-[300px] h-[90vh] bg-white shadow-lg z-40 transform transition-transform duration-300 ${
+          isOpen ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"
+        }`}
+      >
+        <button
+          className="absolute top-4 right-4 text-primary"
+          onClick={closeMenu}
+          aria-label="Close navigation menu"
+        >
+          <X size={28} />
+        </button>
+
+        <nav className="flex flex-col gap-6 font-semibold mt-16 text-navy text-lg p-6">
+          <Link href="/" className="p-2" onClick={closeMenu}>
+            Home
+          </Link>
+          <Link href="/brands" className="p-2" onClick={closeMenu}>
+            Our Brands
+          </Link>
+          <Link href="/blog" className="p-2" onClick={closeMenu}>
+            Blog
+          </Link>
+          <Link href="/careers" className="p-2" onClick={closeMenu}>
+            Careers
+          </Link>
+        </nav>
+      </div>
     </header>
   );
 };
 
 export default MainNavigation;
-
-{
-  /* Mobile Menu (Slide-in) */
-}
-//  <div
-//  className={`fixed inset-0 bg-black bg-opacity-50 z-30 transition-opacity duration-300 ${
-//    isOpen ? "opacity-100 visible" : "opacity-0 invisible"
-//  }`}
-//  onClick={toggleMenu} // Clicking outside closes menu
-// ></div>
-
-// <nav
-//  className={`fixed top-0 right-0 w-3/4 max-w-[300px] h-full bg-white shadow-lg z-40 transform transition-transform duration-300 ${
-//    isOpen ? "translate-x-0" : "translate-x-full"
-//  }`}
-// >
-//  <button
-//    className="absolute top-4 right-4 text-primary"
-//    onClick={toggleMenu}
-//    aria-label="Close navigation menu"
-//  >
-//    <X size={28} />
-//  </button>
-
-//  <div className="flex flex-col items-start mt-16 space-y-6 text-navy text-lg p-6">
-//    <Link href="/" className="p-2" onClick={toggleMenu}>
-//      Home
-//    </Link>
-//    <Link href="/brands" className="p-2" onClick={toggleMenu}>
-//      Our Brands
-//    </Link>
-//    <Link href="/blog" className="p-2" onClick={toggleMenu}>
-//      Blog
-//    </Link>
-//    <Link href="/careers" className="p-2" onClick={toggleMenu}>
-//      Careers
-//    </Link>
-//  </div>
-// </nav>
