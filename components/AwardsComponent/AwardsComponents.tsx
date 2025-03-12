251079dcb05242cb09d64a7dfdf7c0371f1dd299
azeezolasunkanmi <azeezolasunkanmi99@yahoo.com>
2025-03-12 17:03:32 +0100

    bug fix

--- components/AwardsComponent/AwardsComponents.tsx
+++ components/AwardsComponent/AwardsComponents.tsx
@@ -5,7 +5,7 @@ import award2 from "@/public/images/awards/award2.jpg";
 import award3 from "@/public/images/awards/award3.jpg";
 import award4 from "@/public/images/awards/award4.png";
 import award5 from "@/public/images/awards/award5.jpg";
-// import award6 from "@/public/images/awards/award6.jpg";
+import award6 from "@/public/images/awards/award6.jpg";
 import award7 from "@/public/images/awards/award7.jpg";
 import award8 from "@/public/images/awards/award8.jpg";
 import award9 from "@/public/images/awards/award9.jpg";
@@ -13,8 +13,8 @@ import award10 from "@/public/images/awards/award10.jpg";
 import award11 from "@/public/images/awards/award11.jpg";
 import award12 from "@/public/images/awards/award12.png";
 import award13 from "@/public/images/awards/award13.jpg";
-// import award14 from "@/public/images/awards/award14.jpg";
-// import award15 from "@/public/images/awards/award15.png";
+import award14 from "@/public/images/awards/award14.jpg";
+import award15 from "@/public/images/awards/award15.png";
 import award16 from "@/public/images/awards/award16.jpg";
 import award17 from "@/public/images/awards/award17.jpg";
 import award18 from "@/public/images/awards/award18.png";
@@ -25,32 +25,42 @@ import award22 from "@/public/images/awards/award22.png";
 const AwardsComponents = () => {
   // Organize awards by brand categories
   const awardsByBrand = {
-    "Teeling Whiskey": [
+    "Teeling Single Grain": [
+      { src: award15, alt: "Teeling Award" },
       { src: award16, alt: "Teeling Award" },
-      { src: award17, alt: "Teeling Award" },
-      { src: award18, alt: "Teeling Award" },
+      { src: award13, alt: "Teeling Award" },
+    ],
+    "Teeling Small Batch": [
+      { src: award9, alt: "Teeling Award" },
       { src: award10, alt: "Teeling Award" },
       { src: award11, alt: "Teeling Award" },
       { src: award12, alt: "Teeling Award" },
+    ],
+    "Teeling Single Malt": [
+      { src: award18, alt: "Teeling Award" },
+      { src: award14, alt: "Teeling Award" },
+      { src: award17, alt: "Teeling Award" },
+    ],
+    "Teeling Rising Reserve 21 YO": [{ src: award8, alt: "Teeling Award" }],
+    "Teeling 30 YO WHISKEY": [{ src: award7, alt: "Teeling award" }],
+
+    "Teeling Blackpitts": [
+      { src: award19, alt: "Blackpitts Award" },
+      { src: award20, alt: "Blackpitts Award" },
+      { src: award21, alt: "Blackpitts Award" },
+      { src: award22, alt: "Blackpitts Award" },
       { src: award13, alt: "Teeling Award" },
-      { src: award8, alt: "Teeling Award" },
-      { src: award9, alt: "Teeling Award" },
-      { src: award7, alt: "Teeling award" },
     ],
     Courvoisier: [{ src: award2, alt: "Courvoisier Award" }],
     "Bisquit & Dubouché": [
       { src: award3, alt: "B&D Award" },
       { src: award4, alt: "B&D Award" },
     ],
-    "Neft Vodka": [{ src: award5, alt: "Neft Award" }],
-    Blackpitts: [
-      { src: award19, alt: "Blackpitts Award" },
-      { src: award20, alt: "Blackpitts Award" },
-      { src: award21, alt: "Blackpitts Award" },
-      { src: award22, alt: "Blackpitts Award" },
-      //   { src: award14, alt: "Teeling Award" },
-      //   { src: award15, alt: "Teeling Award" },
+    "Neft Vodka": [
+      { src: award5, alt: "Neft Award" },
+      { src: award6, alt: "Neft Award" },
     ],
+
     "Padre Azul": [{ src: award1, alt: "Padre Award" }],
   };
 
@@ -60,7 +70,7 @@ const AwardsComponents = () => {
         <h2 className="top-header text-center">Awards</h2>
         <p className="text-center text-white max-w-2xl mx-auto mb-10">
           The brands in our portfolio have been recognized globally for their
-          excellence, earning prestigious awards in the wine and spirits
+          excellence, earning prestigious awards in the wines and spirit
           industry. Each accolade reflects the craftsmanship, quality, and
           dedication behind every bottle.
         </p>
